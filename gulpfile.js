242d2369287b3eb887dff64816c4c0f23737d204
vikingleo <viking657@hotmail.com>
2019-03-30 23:52:40 +0800

    添加拷贝静态资源

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,12 @@ var gulp = require('gulp'),
     cleanCSS = require('gulp-clean-css'),
     minify = require('gulp-minify')
 
+// 复制必须文件到dist
+gulp.task('copy', function () {
+    return gulp.src(['src/assets/**/*', '!./src/assets/scss/**', '!./src/assets/basic-style/**'])
+        .pipe(gulp.dest('dist')), gulp.src('dist/**/*')
+        .pipe(gulp.dest('dist'))
+});
 // html文件合成
 gulp.task('fileInclude', function () {
     return gulp.src('src/pages/**/*.html')
@@ -20,7 +26,7 @@ gulp.task('fileInclude', function () {
 });
 //scss编译
 gulp.task('scss', function () {
-    return gulp.src('src/assets/css/style.scss')
+    return gulp.src('src/assets/scss/*.scss')
         .pipe(sass())
         .pipe(plumber())
         .pipe(autoprefixer({
@@ -34,7 +40,7 @@ gulp.task('scss', function () {
 //监听
 gulp.task('watch', function () {
     // 监听src内所有html
-    gulp.watch('src/**/*.html', gulp.series('fileInclude'));
+    gulp.watch('src/**/*.html', gulp.series(['fileInclude', 'copy']));
     // 监听src内所有css
-    gulp.watch('src/**/*.scss', gulp.series('scss'));
+    gulp.watch('src/**/*.scss', gulp.series(['scss', 'copy']));
 });
