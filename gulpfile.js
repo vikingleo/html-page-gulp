c837795508a39b7850d3327c6865e795a8529f40
vikingleo <viking657@hotmail.com>
2020-01-06 22:22:49 +0800

    完成gulpfile的watch

--- gulpfile.js
+++ gulpfile.js
@@ -8,101 +8,104 @@ var gulp = require('gulp'),
     sourcemaps = require('gulp-sourcemaps'),
     babel = require('gulp-babel'),
     ts = require('gulp-typescript'),
-    rev = require('gulp-rev'),
-    revCollector = require('gulp-rev-collector')
+    revAll = require('gulp-rev-all'),
+    clean = require('gulp-clean')
 
-// 复制必须文件到dist
-const copyImgUrl = ['src/assets/images']
-const copyJsUrl = ['src/assets/js/*.js', 'src/assets/js/plugins']
-const copyCss = ['src/assets/css/*.css']
+// 复制无需打包的库文件到dist
+const copyUrl = [ 'src/assets/js/lib/**/*' ]
 gulp.task('copy', () =>
-    gulp.src(copyImgUrl).pipe(gulp.dest('dist/assets')),
-    gulp.src(copyJsUrl).pipe(gulp.dest('dist/assets/js')),
-    gulp.src(copyCss).pipe(gulp.dest('dist/assets/css'))
-);
+    gulp.src(copyUrl).pipe(gulp.dest('dist/assets/js/lib'))
+)
 // html文件合成
 gulp.task('generateHtml', function () {
     return gulp.src('src/pages/**/*.html')
-        .pipe(fileInclude({
-            prefix: '@@',
-            basepath: '@file'
-        }))
-        .pipe(minify({
-            comments: true
-        }))
-        .pipe(gulp.dest('dist'))
-});
+    .pipe(fileInclude({
+        prefix: '@@',
+        basepath: '@file'
+    }))
+    .pipe(minify({
+        comments: true
+    }))
+    .pipe(gulp.dest('dist'))
+})
 //scss编译
 gulp.task('generateScss', function () {
-    return gulp.src('src/assets/css/*.scss')
+    return gulp.src([ 'src/assets/css/style.scss' ])
+    .pipe(sass())
+    .pipe(plumber())
+    .pipe(autoprefixer({
+        overrideBrowserslist: [ 'last 2 versions' ],
+        cascade: false
+    }))
+    .pipe(sourcemaps.init())
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('dist/assets/css')),
+        // 插件css
+        gulp.src([ 'src/assets/js/plugins/**/*.scss' ])
         .pipe(sass())
         .pipe(plumber())
         .pipe(autoprefixer({
-            overrideBrowserslist: ['last 2 versions'],
+            overrideBrowserslist: [ 'last 2 versions' ],
             cascade: false
         }))
         .pipe(sourcemaps.init())
         .pipe(cleanCSS())
-        // .pipe(sourcemaps.write())
-        .pipe(rev())
-        .pipe(gulp.dest('dist/assets/css'))
-        .pipe(rev.manifest())
-        .pipe(gulp.dest('dist/rev'))
-});
+        .pipe(gulp.dest('dist/assets/js/plugins'))
+})
 // 替换md5后缀
-const revUrl = ['dist/rev/*.json', 'dist/*.html']
-gulp.task('rev', () =>
-    gulp.src(revUrl)
-        .pipe(revCollector({ replaceReved: true }))
-        .pipe(gulp.dest('dist'))
-)
-const babelSrc = ['src/**/*.es6']
+gulp.task('rev', () => {
+    return gulp.src([ 'dist/**' ])
+    .pipe(revAll.revision({
+        dontRenameFile: [ 'html', '/lib' ]
+        // debug: true
+    }))
+    .pipe(gulp.dest('./dist/'))
+})
+const babelSrc = [ 'src/**/*.es6' ]
 // babel
 gulp.task('generateBabel', () =>
     gulp.src(babelSrc)
-        .pipe(sourcemaps.init())
-        .pipe(babel({
-            presets: ['@babel/env']
-        }))
-        // .pipe(rename())
-        .pipe(rev())
-        .pipe(gulp.dest('dist'))
-        .pipe(rev.manifest())
-        .pipe(gulp.dest('dist/rev'))
-);
+    .pipe(sourcemaps.init())
+    .pipe(babel({
+        presets: [ '@babel/env' ]
+    }))
+    .pipe(gulp.dest('dist'))
+)
 // ts编译
-const tsUrl = ['src/assets/js/**/*.ts']
+const tsUrl = [ 'src/assets/js/**/*.ts' ]
 gulp.task('generateTs', () => gulp.src(tsUrl)
-    .pipe(ts({ declaration: true }))
-    .pipe(rev())
+    .pipe(ts({declaration: true}))
     .pipe(gulp.dest('dist/assets/js'))
-    .pipe(rev.manifest())
-    .pipe(gulp.dest('dist/rev'))
 )
 
-
-// 生成html且替换css链接
-gulp.task('html', gulp.series(['generateHtml', 'rev']))
-
-// 生成带md5后缀带css文件，并且替换html的后缀
-gulp.task('scss', gulp.series(['generateScss', 'rev']))
-
-// 生成带md5后缀带css文件，并且替换html的后缀
-gulp.task('babel', gulp.series(['generateBabel', 'rev']))
-
-// 生成带md5后缀带css文件，并且替换html的后缀
-gulp.task('ts', gulp.series(['generateTs', 'rev']))
-
+// 清空生成目录
+gulp.task('clean', () =>
+    gulp.src('dist', {read: false, allowEmpty: true})
+    .pipe(clean({force: true}))
+)
 // 默认任务
-gulp.task('default', gulp.parallel(['scss', 'html', 'ts', 'babel', 'copy']))
+gulp.task('default',
+    gulp.series(
+        'clean',
+        gulp.parallel(
+            'generateScss',
+            'generateTs',
+            'generateBabel',
+            'generateHtml'
+        ),
+        'rev',
+        'copy'
+    )
+)
 //监听
 gulp.task('watch', function () {
+    gulp.watch([ 'src/**/*' ], gulp.parallel('default'))
     // 监听src内所有html
-    gulp.watch('src/**/*.html', gulp.series(['scss', 'html']));
-    // 监听src内所有css
-    gulp.watch('src/**/*.scss', gulp.series(['scss', 'html']));
-    // 监听src内所有ts
-    gulp.watch('src/**/*.ts', gulp.series(['ts', 'html']))
-    // 监听src内所有es6
-    gulp.watch('src/**/*.es6', gulp.series(['babel', 'html']))
-});
+    // gulp.watch('src/**/*.html', 'html')
+    // // 监听src内所有css
+    // gulp.watch('src/**/*.scss', 'scss')
+    // // 监听src内所有ts
+    // gulp.watch('src/**/*.ts', 'ts')
+    // // 监听src内所有es6
+    // gulp.watch('src/**/*.es6', 'babel')
+})
